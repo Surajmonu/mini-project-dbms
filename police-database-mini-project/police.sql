f15d046bc254e65b24b9eb93f71c2e1d4018775f
Surajmonu <75153609+Surajmonu@users.noreply.github.com>
2021-01-25 00:29:19 +0530

    Add files via upload

--- police-database-mini-project/police.sql
+++ police-database-mini-project/police.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 21, 2021 at 05:54 PM
--- Server version: 5.7.14
--- PHP Version: 7.0.10
+-- Generation Time: Jan 24, 2021 at 07:50 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -30,7 +31,7 @@ CREATE TABLE `case_data` (
   `case_id` int(8) NOT NULL,
   `case_name` varchar(30) NOT NULL,
   `description` varchar(220) NOT NULL,
-  `date_update` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `date_update` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
 --
@@ -41,7 +42,8 @@ INSERT INTO `case_data` (`case_id`, `case_name`, `description`, `date_update`) V
 (1, 'snach', 'chain snaching', '2021-01-21 23:01:05'),
 (2, 'robbery', 'house store robbery', '2021-01-21 23:01:51'),
 (3, 'murder', 'murder some people', '2021-01-21 23:03:55'),
-(4, 'over speed', 'riding vehicle at high speed', '2021-01-21 23:04:52');
+(4, 'over speed', 'riding vehicle at high speed', '2021-01-21 23:04:52'),
+(10, 'laps', 'uuyth', '2021-01-23 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -54,7 +56,7 @@ CREATE TABLE `complaints` (
   `cmp_name` varchar(30) NOT NULL,
   `case_no` int(11) NOT NULL,
   `Description` varchar(220) NOT NULL,
-  `date_reg` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `date_reg` datetime DEFAULT NULL ON UPDATE current_timestamp(),
   `police_handle` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
@@ -75,7 +77,7 @@ CREATE TABLE `criminal` (
   `c_id` int(11) NOT NULL,
   `c_name` varchar(30) NOT NULL,
   `case_no` int(11) NOT NULL,
-  `date_committed` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `date_committed` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
 --
@@ -84,7 +86,9 @@ CREATE TABLE `criminal` (
 
 INSERT INTO `criminal` (`c_id`, `c_name`, `case_no`, `date_committed`) VALUES
 (1, 'john', 2, '2021-01-21 23:10:36'),
-(2, 'jame', 3, '2021-01-21 23:10:36');
+(2, 'jame', 3, '2021-01-21 23:10:36'),
+(10, 'ijhu', 76, '2021-01-08 00:00:00'),
+(11, 'suraj', 22, '2021-01-14 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -93,10 +97,10 @@ INSERT INTO `criminal` (`c_id`, `c_name`, `case_no`, `date_committed`) VALUES
 --
 
 CREATE TABLE `police_data` (
-  `id` int(8) NOT NULL,
-  `name` varchar(30) NOT NULL,
-  `design` varchar(220) NOT NULL,
-  `case_handle` int(10) NOT NULL
+  `id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `design` varchar(20) NOT NULL,
+  `case_handle` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
 --
@@ -104,7 +108,8 @@ CREATE TABLE `police_data` (
 --
 
 INSERT INTO `police_data` (`id`, `name`, `design`, `case_handle`) VALUES
-(1, 'Ram', 'SI', 1);
+(0, 'suneesh', '4er', 56),
+(89, 'eeee', '4e4', 45);
 
 -- --------------------------------------------------------
 
@@ -116,17 +121,9 @@ CREATE TABLE `prison` (
   `prisoner_id` int(11) NOT NULL,
   `prisoner_name` varchar(30) NOT NULL,
   `case_no` int(11) NOT NULL,
-  `date_arrest` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `date_arrest` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `date_release` date NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `prison`
---
-
-INSERT INTO `prison` (`prisoner_id`, `prisoner_name`, `case_no`, `date_arrest`, `date_release`) VALUES
-(1, 'frddy', 2, '2021-01-21 23:12:37', '2021-10-22'),
-(2, 'robby', 4, '2021-01-21 23:12:37', '2022-12-23');
 
 --
 -- Indexes for dumped tables
@@ -166,6 +163,17 @@ ALTER TABLE `police_data`
 ALTER TABLE `prison`
   ADD PRIMARY KEY (`prisoner_id`),
   ADD KEY `case_no` (`case_no`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `criminal`
+--
+ALTER TABLE `criminal`
+  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
